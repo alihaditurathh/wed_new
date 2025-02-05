8102fcb65462c1642f696ebf7a3ada5a724d57c2
Ali Hadi <alialihadi.a123@gmail.com>
2025-02-05 14:22:49 +0300

    firebace android setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:wed_for_weddings/wed_app.dart';
 
  Future<void> main() async {
     WidgetsFlutterBinding.ensureInitialized();
-
-
+  Platform.isAndroid
+? await Firebase.initializeApp(
+ options: const FirebaseOptions(
+  apiKey:'AIzaSyAzim_OqwzhTrlVtV7TfLp6fFtDQcgQYTg',
+  appId:'1:595935708915:android:96260646d70a78d13a5986',
+  messagingSenderId:'595935708915',
+  projectId:'wedweddings-bba88',
+ )
+):    
+ await Firebase.initializeApp();
 
     runApp(const Wed());
  }
