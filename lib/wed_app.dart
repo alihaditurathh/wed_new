81fcf88a3c8fc1657ee3e36b3cdfabaa790add02
Ali Hadi <alialihadi.a123@gmail.com>
2025-02-05 20:40:27 +0300

    env created done

--- lib/wed_app.dart
+++ lib/wed_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wed_for_weddings/core/app/env.variables.dart';
 
 class Wed extends StatelessWidget {
   const Wed({super.key});
@@ -7,10 +8,11 @@ class Wed extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Wed',
+        debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
       theme: ThemeData.light(useMaterial3: true),
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.blue,
+          backgroundColor: Colors.pink,
           title:const Text('Wed'),
           centerTitle: true,
         ),
