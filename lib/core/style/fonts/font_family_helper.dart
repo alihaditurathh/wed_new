0c73efa641680205cd290fdc0ea71fe6457e18d0
Ali Hadi <alialihadi.a123@gmail.com>
2025-02-11 13:35:39 +0300

    font family and weight done

---
+++ lib/core/style/fonts/font_family_helper.dart
@@ -0,0 +1,11 @@
+
+//TODO سويج بين لغة العربية والانكليزية
+class FontFamilyHelper {
+  const FontFamilyHelper._();
+
+  static const String cairoArabic = 'Cairo';
+
+  static const String poppinsEnglish = 'Poppins';
+
+
+}
