7d43921e5de441f99367e79ce3051a217f416b9f
Ali Hadi <alialihadi.a123@gmail.com>
2025-02-22 11:37:01 +0300

    setup frezzed, flutter

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wed_for_weddings/core/app/bloc_observer.dart';
 import 'package:wed_for_weddings/core/app/env.variables.dart';
 import 'package:wed_for_weddings/wed_app.dart';
 
@@ -19,6 +21,7 @@ import 'package:wed_for_weddings/wed_app.dart';
   projectId:'wedweddings-bba88',
  )
  ,):  await Firebase.initializeApp();
+ Bloc.observer=AppBlocObserver();
 //لايقاف تدوير الشاشة
   await SystemChrome.setPreferredOrientations(
     [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp],
