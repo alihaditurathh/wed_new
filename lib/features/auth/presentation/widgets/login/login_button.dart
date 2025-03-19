c7e50b2a3748824b075a862a898699aef65a240e
Ali Hadi <alialihadi.a123@gmail.com>
2025-03-20 00:19:39 +0300

    login done

--- lib/features/auth/presentation/widgets/login/login_button.dart
+++ lib/features/auth/presentation/widgets/login/login_button.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wed_for_weddings/core/common/animations/animate_do.dart';
+import 'package:wed_for_weddings/core/common/toast/show_toast.dart';
 import 'package:wed_for_weddings/core/common/widgets/custom_linear_button.dart';
 import 'package:wed_for_weddings/core/common/widgets/text_app.dart';
 import 'package:wed_for_weddings/core/extensions/context_extension.dart';
 import 'package:wed_for_weddings/core/language/lang_keys.dart';
+import 'package:wed_for_weddings/core/routes/app_routes.dart';
 import 'package:wed_for_weddings/core/style/fonts/font_weight_helper.dart';
+import 'package:wed_for_weddings/features/auth/presentation/bloc/auth_bloc.dart';
 
 class LoginButton extends StatelessWidget {
   const LoginButton({super.key});
@@ -13,16 +17,61 @@ class LoginButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return CustomFadeInRight(
       duration: 600,
-      child: CustomLinearButton(
-        onPressed: () {},
-        height: 50,
-        width: MediaQuery.of(context).size.width,
-        child: TextApp(
-          text: context.translate(LangKeys.login),
-          theme: context.textStyle
-              .copyWith(fontSize: 18, fontWeight: FontWeightHelper.bold),
-        ),
+      child: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          state.whenOrNull(
+            success: (userRole) {
+                 ShowToast.showToastSuccessTop(
+                message: context.translate(LangKeys.loggedSuccessfully),
+              );
+              if (userRole == 'admin') {
+                context.pushNamedAndRemoveUntil(AppRoutes.homeAdmin);
+              } else {
+                context.pushNamedAndRemoveUntil(AppRoutes.homeCustomer);
+              }
+            },
+            error: (messsage) {
+              ShowToast.showToastErrorTop(
+                message: context.translate(messsage),
+              );
+            },
+          );
+        },
+        builder: (context, state) {
+          return state.maybeWhen(
+            loading: () {
+              return CustomLinearButton(
+                onPressed: () {},
+                height: 50,
+                width: MediaQuery.of(context).size.width,
+                child: const CircularProgressIndicator(
+                  color: Colors.white,
+                ),
+              );
+            },
+            orElse: () {
+              return CustomLinearButton(
+                onPressed: () {
+                  _validateThenDoLogin(context);
+                },
+                height: 50,
+                width: MediaQuery.of(context).size.width,
+                child: TextApp(
+                  text: context.translate(LangKeys.login),
+                  theme: context.textStyle.copyWith(
+                      fontSize: 18, fontWeight: FontWeightHelper.bold),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
+  }
+
+  void _validateThenDoLogin(BuildContext context) {
+    if (context.read<AuthBloc>().formKey.currentState!.validate()) {
+      context.read<AuthBloc>().add(const AuthEvent.login());
+    }
   }
 }
--- lib/wed_app.dart
+++ lib/wed_app.dart
@@ -62,7 +62,13 @@ class Wed extends StatelessWidget {
                       );
                     },
                     onGenerateRoute: AppRoutes.onGenerateRoute,
-                    initialRoute: AppRoutes.login,
+                    initialRoute: SharedPref()
+                                .getString(PrefKeys.accessToken) !=
+                            null
+                        ? SharedPref().getString(PrefKeys.userRole) != 'admin'
+                            ? AppRoutes.homeCustomer
+                            : AppRoutes.homeAdmin
+                        : AppRoutes.login,
                   );
                 },
               ),
